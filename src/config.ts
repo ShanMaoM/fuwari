9f962416e8e2c2c0dc5a40d79f33048e12c92983
ShanMaoM <zhaoqingdong527@gmail.com>
2025-08-11 17:23:15 +0800

    项目初始化

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Save Keep Contiue",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 210, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
@@ -32,9 +32,9 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		 {
-		   src: '/favicon/favicon.ico',    // Path of the favicon, relative to the /public directory
-		   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		   src: 'https://pic.9wt.cn/uploads/20250811/fb8f3e228e00f0240610b6842ae0f2de.webp',    // Path of the favicon, relative to the /public directory
+		  // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		 //  sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		 }
 	],
 };
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/1.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https://pic.9wt.cn/uploads/20250811/40853ec703657f8cb5950e00f86c97bd.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "ShanMaoy",
 	bio: "救赎",
 	links: [
